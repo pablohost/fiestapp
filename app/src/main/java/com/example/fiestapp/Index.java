3dd5f22af04e644673679a6ce2a445e4e0b27e49
Pablo G. Henriquez <pablo.gallardo20@inacapmail.cl>
2019-09-13 03:47:19 -0300

    Creacion pantalla login

--- app/src/main/java/com/example/fiestapp/Index.java
+++ app/src/main/java/com/example/fiestapp/Index.java
@@ -2,7 +2,9 @@ package com.example.fiestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Index extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Index extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+    }
+    public void login(View v){
+        Intent i=new Intent(this, Login.class);
+        startActivity(i);
     }
 }
