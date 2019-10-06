1264882994e84cbc0ecc19cea23546596d1810f4
Pablo G. Henriquez <pablo.gallardo20@inacapmail.cl>
2019-10-06 03:03:56 -0300

    Diseño de la cabecera con video y formulario

---
+++ script/jsIndex.js
@@ -0,0 +1,14 @@
+$(function(){
+        
+  //********animacion del header typed.js*********
+
+  var options = {
+    strings: ['LAS MEJORES FIESTAS', 'EL MEJOR CARRETE', 'SHOWS DE STAND UP COMEDY', 'FESTIVALES DE MUSICA', 'TU MEJOR PANORAMA'],
+    typeSpeed: 100,
+    loop: true
+  };
+
+  var typed = new Typed('#headCute', options);
+
+      
+});
